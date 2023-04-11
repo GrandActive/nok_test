5870db5a742dc60da4a3285e26969fd04eafc66d
artyommukhin <artyom.mukhin@gmail.com>
2023-04-11 18:02:48 +0700

    Use case for generating questions list for test

---
+++ lib/training/domain/get_random_questions_for_test.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+import 'package:gip_test/training/domain/model/test_question.dart';
+import 'package:gip_test/training/data/questions_repository.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class GetRandomQuestionsForTest {
+  GetRandomQuestionsForTest({required this.repository});
+
+  final QuestionsRepository repository;
+
+  Future<List<TestQuestion>> call({int count = 3}) async {
+    final questions = (await repository.getAllQuestions())
+        .map((question) => TestQuestion(source: question, isAnsweredCorrectly: null))
+        .toList(growable: false);
+    final random = Random();
+    final result = <TestQuestion>[];
+    final selected = <int>[];
+    for (var i = 0; i < count; i++) {
+      int nextIndex;
+      do {
+        nextIndex = random.nextInt(questions.length);
+      } while (selected.contains(nextIndex));
+      selected.add(nextIndex);
+      result.add(questions[nextIndex]);
+    }
+    return result;
+  }
+}
