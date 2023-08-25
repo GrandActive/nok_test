88f37fc9fc7791ac85b2f7c167e411b1758fe55b
artyommukhin <artyom.mukhin@gmail.com>
2023-08-25 21:08:55 +1100

    Deleted local database, using updated backend database

--- lib/testing/data/firebase_data_source.dart
+++ lib/testing/data/firebase_data_source.dart
@@ -1,24 +1,26 @@
 import 'dart:convert';
 
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/services.dart';
 import 'package:nok_test/testing/data/model/question.dart';
 import 'package:injectable/injectable.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 @injectable
 class FirebaseDataSource {
   FirebaseDatabase get _database => FirebaseDatabase.instance;
 
   Future<List<Question>?> getAllQuestions(String topic) async {
-    final fileContents = await rootBundle.loadString("assets/sample_db_v1.1.0.json");
-    final Map data = jsonDecode(fileContents);
+    await _checkVersion();
+
+    final dataEvent = await _database.ref().once();
+    final Map data = jsonDecode(jsonEncode(dataEvent.snapshot.value));
 
     final Map questions = data["questions"];
     final Map topics = data["topics"];
 
-    final questionsIds = (topics[topic]["questions"] as Map).entries;
-    return questionsIds.map((id) {
-      final question = questions[id.value];
+    final questionsIds = (topics[topic]["questions"] as List);
+    return questionsIds.whereType<String>().map((id) {
+      final question = questions[id];
       switch (question["type"]) {
         case "selection":
           return SelectionQuestion.fromJson(question);
@@ -27,10 +29,33 @@ class FirebaseDataSource {
         case "sequence":
           return SequenceQuestion.fromJson(question);
         case "matching":
-          return MatchingQuestion.fromJson(question);
+          final preparedQuestion = _prepareMatchingQuestion(question);
+          return MatchingQuestion.fromJson(preparedQuestion);
         default:
           throw Exception("Unknown question type in database: ${question["type"]}");
       }
     }).toList();
   }
+
+  dynamic _prepareMatchingQuestion(dynamic question) {
+    final correctMatchList = question["correctMatch"] as List;
+    final correctMatchMap = <String, dynamic>{};
+    for (var i = 1; i < correctMatchList.length; i++) {
+      correctMatchMap[i.toString()] = correctMatchList[i];
+    }
+    question["correctMatch"] = correctMatchMap;
+    return question;
+  }
+
+  Future<void> _checkVersion() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    final appVersion = packageInfo.version;
+
+    final dbVersionEvent = await _database.ref("version").once();
+    final dbVersion = dbVersionEvent.snapshot.value as String;
+
+    if (dbVersion != appVersion) throw WrongDbVersionException();
+  }
 }
+
+class WrongDbVersionException implements Exception {}
