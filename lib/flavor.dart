0783f82522038db7dbcf6eaf4662cab3be91a9f1
Artyom Mukhin <artyom.mukhin@gmail.com>
2023-11-03 01:43:35 +1100

    Revert "Adding flavors"

--- lib/flavor.dart
+++
@@ -1,7 +0,0 @@
-/// Current flavor of the app.
-///
-/// Define this variable during build
-/// using *--dart-define=app.flavor=value* syntax.
-///
-/// Possible values are *free* and *paid*.
-const flavor = String.fromEnvironment('app.flavor');
