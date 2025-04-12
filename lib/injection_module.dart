70c09720fa954a7ebf90f00ecff945cc2cb49084
artyommukhin <artyom.mukhin@gmail.com>
2025-04-13 01:04:52 +1100

    Add logger for HTTP requests

--- lib/injection_module.dart
+++ lib/injection_module.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:nok_test/payments/api/payments_api.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 @module
 abstract class InjectionModule {
-  Dio get dio => Dio();
+  Dio get dio => Dio()..interceptors.add(PrettyDioLogger(enabled: kDebugMode));
 
   PaymentsApi get paymentsApi => PaymentsApi(dio);
 }
